8d1344ad3fede0241e98f93437d75e047d389c3e
blach.b <b.blachowski@wp.pl>
2017-01-21 18:57:19 +0100

    dodano 3 fightera

---
+++ src/com/main/Testowa.java
@@ -0,0 +1,19 @@
+package com.main;
+
+public class Testowa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String monkeys [] = new String [3];
+		monkeys[0]="stupid George";
+		monkeys[1]="Fat Albert";
+		monkeys[1]="Irrevelant someone";
+		for (int i=0; i<monkeys.length; i++){
+			System.out.println(monkeys[i]);
+		}
+	}
+
+}
